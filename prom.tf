98e464295d811609cc166c907b6f2ca4c1a05a9a
AnAlligatorPear <vitaemorte@gmail.com>
2025-02-12 15:57:49 -0500

    Removed commented code from .tptl file to preventi issues. Added regexes for hostname and port extraction.

--- prom.tf
+++ prom.tf
@@ -8,6 +8,18 @@ resource "kubernetes_namespace" "prom" {
   }
 }
 
+
+# Extract Hostname and Port
+locals {
+  vault_url      = var.vault_address
+  vault_hostname = regex("^https?://([^/:]+)", var.vault_address)[0]
+  vault_port     = length(regexall(":(\\d+)", var.vault_address)) > 0 ? regexall(":(\\d+)", var.vault_address)[0][0] : "8200"
+  protocol       = can(regex("^https?", var.vault_address)[0]) ? regex("^https?", var.vault_address)[0] : "http"
+
+}
+
+
+
 # Deploying Prometheus + Grafana 
 resource "helm_release" "prometheus" {
   name       = "prometheus"
@@ -19,18 +31,14 @@ resource "helm_release" "prometheus" {
   # Load the existing YAML configuration and marge the templatefile with the prometheus yaml
   values = [
     templatefile("${path.module}/prom.stack.values.yml.tftpl", {
-      vault_address = var.vault_address
-      vault_token   = var.vault_token
+      vault_address  = var.vault_address
+      vault_token    = var.vault_token
+      vault_hostname = local.vault_hostname
+      vault_port     = local.vault_port
     })
   ]
-}
-/*
-  values = [
-    "${file("prom.stack.values.yml")}"
-  ]
-  }
 
-*/
+}
 
 
 resource "kubernetes_secret_v1" "vault_token" {
@@ -38,12 +46,25 @@ resource "kubernetes_secret_v1" "vault_token" {
     name      = "vaulttoken"
     namespace = kubernetes_namespace.prom.id
   }
-
   data = {
     token = var.vault_token
   }
+  type = "Opaque" # 
 
-  type = "kubernetes.io/opaque"
+
+}
+
+resource "kubernetes_secret_v1" "vault_address" {
+  metadata {
+    name      = "vaultaddress"
+    namespace = kubernetes_namespace.prom.id
+  }
+  data = {
+    address = var.vault_address
+  }
+  type = "Opaque"
+
+
 }
 
 
--- providers.tf
+++ providers.tf
@@ -9,8 +9,11 @@ terraform {
       source  = "hashicorp/kubernetes"
       version = "~> 2.35.1"
     }
+
   }
 }
+
+
 
 provider "kubernetes" {
   config_path    = "~/.kube/config"
